7908083f80ac0c0875a755b129b5e71317dddb9e
Adonis Veloz <20140452@ce.pucmm.edu.do>
2018-07-22 17:29:31 -0400

    Subiendo rutas faltantes.

---
+++ src/main/java/main/Filtros.java
@@ -0,0 +1,7 @@
+package main;
+
+public class Filtros {
+    public void aplicarFiltros(){
+
+    }
+}
--- src/main/java/main/RutasSpark.java
+++ src/main/java/main/RutasSpark.java
@@ -1,12 +1,55 @@
 package main;
 
 import freemarker.template.Configuration;
+import logical.Usuario;
+import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class RutasSpark {
-    public void iniciarSpark(){
+    public void iniciarSpark() {
         Configuration cfg = new Configuration(Configuration.VERSION_2_3_0);
         cfg.setClassForTemplateLoading(Main.class, "/templates");
         FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine(cfg);
+
+        List<Usuario> misUsuarios = new ArrayList<Usuario>();
+
+
+        get("/login", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+
+            return new ModelAndView(attributes, "sign-in.ftl");
+        }, freeMarkerEngine);
+
+        post("/procesarUsuario", (request, response) -> {
+            try {
+                String usernameAVerificar = request.queryParams("username");
+                String passwordsAVerificar = request.queryParams("password");
+                //String isRecordado = request.queryParams("recordar");
+
+                if (usernameAVerificar != null && passwordsAVerificar != null)
+                    response.redirect("/perfilUsuario");
+                else
+                    response.redirect("/login");
+
+            } catch (Exception e) {
+                System.out.println("Error al intentar iniciar sesión " + e.toString());
+            }
+            return "";
+        });
+
+        get("/perfilUsuario", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+
+            return new ModelAndView(attributes, "user-profile.ftl");
+        }, freeMarkerEngine);
     }
 }
+
